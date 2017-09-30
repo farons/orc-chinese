95099acfbe5747569be87c96b95bc41f34a6c32a
farons <yanzhilongll@163.com>
2017-09-30 17:12:17 +0800

    add 61.jpg test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from operation import PATH_OCR, PATH_PIC, PATH_TESSDATE
 PATH_OCR = r'C:/Program Files/Tesseract-OCR/tesseract'
 PATH_TESSDATE = r'C:/Program Files/Tesseract-OCR/tessdata'
 
-PATH_PIC = r'TEST/picture/61.jpg'
+PATH_PIC = r'TEST/picture/61.bmp'
 OPEN_FIlE = r'TEST/result/init/61.txt'
 WRITE_FIlE = r'TEST/result/clear/61.txt'
 LAST_FILE = r'TEST/result/standard/61.txt'
@@ -22,5 +22,3 @@ file.writeFile(code,OPEN_FIlE)      # write in init
 file.clearBL(OPEN_FIlE,WRITE_FIlE)      # write in clear
 
 file.standardFile(WRITE_FIlE, LAST_FILE)
-data = readImage.getData(LAST_FILE)        # get DATA= {"ID":"","title":""}
-print(data)
